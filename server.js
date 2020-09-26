0fd85c85ad81b7eb139bb3ee32901abdf04ede85
rohan.agarwal <agarwal.rohan80@yahoo.com>
2020-09-27 02:06:27 +0530

    initialized first view

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const path = require('path');
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
+    res.render('room.ejs');
 })
-
 
 server.listen(3030);
