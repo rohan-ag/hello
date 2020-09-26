316f5c5c0f3321e0493cb48866dee5ac1cc32be0
rohan.agarwal <agarwal.rohan80@yahoo.com>
2020-09-27 02:30:58 +0530

    redirecing user to a new room with unique id

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const path = require('path');
+
+const {
+    v4: uuidv4
+} = require('uuid');
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('room.ejs');
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:roomId', (req, res) => {
+    res.render('room', {
+        roomId: req.params.roomId
+    });
 })
 
 server.listen(3030);
