2494a852f88ab1ebe4c2db8d210fb85735bae3f2
rohan.agarwal <agarwal.rohan80@yahoo.com>
2020-09-28 00:56:59 +0530

    display a new caller stream to all existing user

--- public/script.js
+++ public/script.js
@@ -12,24 +12,46 @@ myVideo.muted = true;
 let videoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
+    audio: false
 }).then(stream => {
     videoStream = stream;
     addVideoStream(myVideo, stream);
+
+    /*
+        Receives Call event . Answers the call and pass the callee stream. Also receives the caller stream and adds to the view.
+    */
+    peer.on('call', call => {
+        call.answer(stream);
+        const videoElement = document.createElement('video');
+        call.on('stream', userVideoStream => addVideoStream(videoElement, userVideoStream))
+    });
+
+    /*
+        Notify when a new user connects  and pass video stream.
+    */
+    socket.on('user-connected', (userId) => {
+        setTimeout(function () {
+            connectNewUser(userId, stream);
+        }, 500)
+    });
+
 }).catch(onCancel => {
     console.log(onCancel)
 })
 
+
+
+/*
+Opens a new Peer connection and emits the join room event with roomId and peerId
+*/
 peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
-})
+});
 
-socket.on('user-connected', (userId) => {
-    connectNewUser(userId);
-})
-
-const connectNewUser = (userId) => {
-    console.log('user has joined', userId)
+const connectNewUser = (userId, myStream) => {
+    const call = peer.call(userId, myStream);
+    const videoElement = document.createElement('video');
+    call.on('stream', userVideoStream => addVideoStream(videoElement, userVideoStream))
 }
 
 const addVideoStream = (video, stream) => {
